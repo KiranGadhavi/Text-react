b3e5c033aad5c922f3d67b4ba950b89b65526d1f
Kiran Gadhavi <kirangadhavi@hotmail.com>
2023-10-26 17:39:00 +0100

    add enable dark mode,remove extrace space button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 // import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
-function App() {
+  function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode=()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='grey';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+
+  }
   return (
 <>
-    { <Navbar title='Text-react' aboutText='About Us'/>}
- <TextForm heading='Enter text below to Analyze'/>
+  <Navbar title='Text-react' aboutText='About Us' mode={mode} toggleMode={toggleMode}/>
+  <TextForm heading='Enter text below to Analyze' mode={mode}/>
+  <About mode={mode} />
      {/* <Navbar title='Text-react' /> */}
     {/* <Navbar/>*/}
-   </>  );
+   </>  
+   );
 }
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,34 +7,53 @@ export default function TextForm(props) {
        let newText=text.toUpperCase();
         setText(newText)   
     }
-    const handleClick=()=>{
+    const handlelwClick=()=>{
         let lowText=text.toLowerCase();
         setText(lowText)
     }
+
+    const handleClick=()=>{
+        let clearText='' ;
+        setText(clearText)                
+    }
+    const handleCopy=()=>{
+     let text=document.getElementById('myBox');
+            text.select();
+      navigator.clipboard.writeText(text.value)         
+  }
+  const handleSpace=()=>{
+    let textSpace=text.split(/[ ]+/)
+    setText(textSpace.join(' '))
+
+ }
     const handleOnChange=(event)=>{
         //console.log('On Change');
         setText(event.target.value)
     }
     const [text, setText] = useState('');
+   // const [msg, setMsg] = useState('');
    // Text='new text';//Incorrect way to change the state
    // setText('new text');//correct way to change the state
    // console.log('')
   return (
     <>
-    <div className='container'>
+    <div className='container'  style={{color:props.mode==='dark'?'white':'black'}}>
      <h1 >{props.heading}</h1>
   <div className="mb-3">
-<textarea className='form-control' value= {text} onChange={handleOnChange} id='myBox' rows='8'></textarea>
+<textarea className='form-control' value= {text} onChange={handleOnChange} id='myBox' rows='8' style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}></textarea>
   </div> 
   <button  className="btn btn-success mx-2" onClick={handleUpClick}>Convert to upper case</button>
-  <button  className="btn btn-success mx-2" onClick={handleClick}>Convert to lower case</button>
+  <button  className="btn btn-success mx-2" onClick={handlelwClick}>Convert to lower case</button>
+  <button  className="btn btn-success mx-2" onClick={handleClick}>Clear Text</button>
+  <button  className="btn btn-success mx-2" onClick={handleCopy}>Copy Text</button>
+  <button  className="btn btn-success mx-2" onClick={handleSpace}>Remove Extra space</button>
 </div>
-<div className='container my-3'>
+<div className='container my-3' style={{color:props.mode==='dark'?'white':'black'}}>
     <h3>Text Summary</h3>
    <p>{text.split(' ').length} words and {text.length} characters</p> 
     <p>{0.008*text.split(' ').length} Minutes Read</p>
     <h3>Preview</h3>
-    <p>{text}</p>
+    <p>{text.length>0?text:'Enter something in the textbox above to preview it here'}</p>
 </div>
 </>
   )
